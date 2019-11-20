3cfbf9e6789ef422c84a50bbd7f80e0819a3c8e3
hala21-meet <52972562+hala21-meet@users.noreply.github.com>
2019-11-20 13:02:28 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,4 +1,6 @@
-class user():
+ list1=[posts]
+ list2=[users]
+ class user():
 	def __init__(self,name,email,password,friends_list,posts):
 		self.name = name
 		self.email= email 
@@ -11,16 +13,41 @@ class user():
 	def remove_friend(self,email):
 		self.friends_list.remove(email)
 		print (self.name + 'has removed' + email + 'as a friend')
-	def posts (self,text):
+	def post (self,text):
 		self.posts.append(text)
 		print (self.name + 'has posted' + text)
-	def get_userInfo (self):
-		print ('Name:' + self.name +'Email:' + self.email + 'Password:'+self.password + 'Friends:' + self.friends + 'Posts:' + self.posts )
-user1= User("Hala","hala99@meet.mit.edu","myhiddenpassword123",[],[])
-user2= User("Grandma","grandma44@meet.mit.edu","honeybooboo",[],[])
+		def get_userInfo (self):
+		print ('Name:' + self.name +'Email:' + self.email + 'Password:'+self.password + 'Friends:' + str (self.friends_list )+ 'Posts:' + str (self.posts) )
+
+		class Post(object):
+			def __init__(self,name,caption,comments=[]):
+				self.name = name 
+				self.caption= caption
+				self.comments=[]
+			def add_caption(self,caption_text):
+				self.caption_text=caption_text 
+				self.caption_text.append(caption_text):
+				print(self.name+"has added"+email + " as a friend")
+			def remove_friend (self,email)
+			self.friends_list.remove(email)
+			print(self.name +_self.name + 
+				"
+
+user1= user("Hala","hala99@meet.mit.edu","myhiddenpassword123",[],[])
+user2= user("Grandma","grandma44@meet.mit.edu","honeybooboo",[],[])
 user1.add_friend('hala99@meet.mit.edu')
 user1.post('snowing')
-user1.get_userInfo()
+user1.get_userInfo()  
+
+
+
+
+
+
+
+
+
+
 
 
 
