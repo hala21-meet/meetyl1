28540e7e72919d99d8be0d272b05ba090e31634e
hala21-meet <52972562+hala21-meet@users.noreply.github.com>
2019-12-18 12:12:00 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,12 +1,13 @@
- list1=[posts]
- list2=[users]
- class user():
+users =[]
+posts =[]
+class user():
 	def __init__(self,name,email,password,friends_list,posts):
 		self.name = name
 		self.email= email 
 		self.password = password 
 		self.friends_list = friends_list
 		self.posts =posts
+		user.append(self)
 	def add_friend(self,email):
 		self.friends_list.append(email)
 		print (self.name + 'has added' + email+ 'as a friend')
@@ -16,22 +17,27 @@
 	def post (self,text):
 		self.posts.append(text)
 		print (self.name + 'has posted' + text)
-		def get_userInfo (self):
-		print ('Name:' + self.name +'Email:' + self.email + 'Password:'+self.password + 'Friends:' + str (self.friends_list )+ 'Posts:' + str (self.posts) )
+		users.append(posts)
+	def get_userInfo (self):
+		print ('Name:' + self.name +'Email:' + self.email + 'Password:'+self.password + 'Friends:' + str (self.friends_list )+ 'Posts:' + str (self.posts))
+class Post(object):
+	def __init__(self,name,caption,comments=[],likes):
+		self.name = name 
+		self.caption= caption
+		self.comment_text=[]
+		self.likes=0
+	def add_caption(self,caption_text):
+		self.caption=caption_text
+		print(self.name+"has added"+ caption_text)
+	def remove_friend (self,email):
+		self.friends_list.remove(email)
+		print(self.name + "has removed"+ email +  " as a friend")
+	def add_comment (self,comment_text):
+		self.comment= comment_text 
+		print(self.name + "has added" + comment_text)  
+	def add_likes (self):
+		pass
 
-		class Post(object):
-			def __init__(self,name,caption,comments=[]):
-				self.name = name 
-				self.caption= caption
-				self.comments=[]
-			def add_caption(self,caption_text):
-				self.caption_text=caption_text 
-				self.caption_text.append(caption_text):
-				print(self.name+"has added"+email + " as a friend")
-			def remove_friend (self,email)
-			self.friends_list.remove(email)
-			print(self.name +_self.name + 
-				"
 
 user1= user("Hala","hala99@meet.mit.edu","myhiddenpassword123",[],[])
 user2= user("Grandma","grandma44@meet.mit.edu","honeybooboo",[],[])
